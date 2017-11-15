ea27005e068455bc595f8b4dc1c6ecbb991887a1
Nikolaj Ivancic <nik@congral.com>
2017-11-15 09:41:39 -0500

    nc

--- skeleton-typescript/after/src/app.ts
+++ skeleton-typescript/after/src/app.ts
@@ -1,11 +1,14 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  configureRouter(config, router) {
+  public router: Router;
+
+  public configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Aurelia';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
-      { route: 'autocomplete',  name: 'autocomplete', moduleId: 'autocomplete', nav: true, title: 'Autocomplete' }
+      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
     ]);
 
     this.router = router;

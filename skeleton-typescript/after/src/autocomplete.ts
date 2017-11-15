777520a63b233d9644263c73be3e55d26c7a2e63
Nikolaj Ivancic <nik@congral.com>
2017-11-15 10:29:07 -0500

    nc

---
+++ skeleton-typescript/after/src/autocomplete.ts
@@ -0,0 +1,16 @@
+import 'kendo-ui/js/kendo.autocomplete';
+
+export class autocomplete{
+  private datasource: kendo.data.DataSource;
+
+  constructor() {
+    this.datasource = new kendo.data.DataSource({
+      transport: {
+        read: {
+          dataType: 'jsonp',
+          url: '//demos.telerik.com/kendo-ui/service/Customers'
+        }
+      }
+    });
+  }    
+}
